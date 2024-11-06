1f17fcfb53c189df1cec816379a19b34cffdf79d
osmr99 <omarsantiagomorales@gmail.com>
2024-11-06 16:57:44 -0600

    Laser Turret Done

--- Assets/LaserTurret.cs
+++ Assets/LaserTurret.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LaserTurret : MonoBehaviour
 {
@@ -14,6 +15,23 @@ public class LaserTurret : MonoBehaviour
 
     List<Vector3> laserPoints = new List<Vector3>();
 
+    GameObject surface;
+    Vector3 newPoint;
+
+    float negX;
+    float negY;
+    float negZ;
+    float negRotationX;
+    float negRotationY;
+    float negRotationZ;
+
+    [SerializeField] bool flipX = true;
+    [SerializeField] bool flipY = true;
+    [SerializeField] bool flipZ = true;
+    //bool flipXtwo = false;
+    //bool flipYtwo = false;
+    //bool flipZtwo = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +41,27 @@ public class LaserTurret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("flipXX"))
+        {
+            //flipXtwo = !flipXtwo;
+            //flipX = !flipX;
+        }
+
+        if (Input.GetButtonDown("flipYY"))
+        {
+            //flipYtwo = !flipYtwo;
+            //flipY = !flipY;
+        }
+
+        if (Input.GetButtonDown("flipZZ"))
+        {
+            //flipZtwo = !flipZtwo;
+            //flipZ = !flipZ;
+        }
+            
+
+
+
         TrackMouse();
         TurnBase();
 
@@ -32,13 +71,89 @@ public class LaserTurret : MonoBehaviour
         if(Physics.Raycast(barrelEnd.position, barrelEnd.forward, out RaycastHit hit, 1000.0f, targetLayer))
         {
             laserPoints.Add(hit.point);
+            if(surface != null)
+            {
+                if (flipX)
+                {
+                    negX = -hit.point.x * 6;
+                    negRotationX = -hit.transform.rotation.x;
+                }
+                else
+                {
+                    negX = hit.point.x + hit.point.x + hit.point.x + hit.point.x + hit.point.x + hit.point.x;
+                    negRotationX = hit.transform.rotation.x;
+                }
+
+
+                if (flipY && surface.transform.position.y != 0)
+                {
+                    negY = -hit.point.y * 6;
+                    negRotationY = -hit.transform.rotation.y;
+                }
+                else if (!flipY && surface.transform.rotation.x == 0 && surface.transform.rotation.y == 0 && surface.transform.rotation.z == 0)
+                {
+                    negY = Mathf.Abs(hit.point.y * 1000000000000000002);
+                    negRotationY = -hit.transform.rotation.y;
+                }
+                else
+                {
+                    negY = hit.point.y + hit.point.y + hit.point.y + hit.point.y + hit.point.y + hit.point.y;
+                    negRotationY = hit.transform.rotation.y;
+                }
+
+                if (flipZ)
+                {
+                    negZ = -hit.point.z * 6;
+                    negRotationZ = -hit.transform.rotation.z;
+                }
+                else
+                {
+                    negZ = hit.point.z + hit.point.z + hit.point.z + hit.point.z + hit.point.z + hit.point.z;
+                    negRotationZ = hit.transform.rotation.z;
+                }
+            }
+
+
+
+            if (surface != null)
+            {
+                newPoint = new Vector3(negX, negY, negZ);
+                laserPoints.Add(newPoint);
+            }
         }
 
         line.positionCount = laserPoints.Count;
+
+        if(hit.collider != null)
+        {
+            surface = hit.collider.gameObject;
+
+            if ((surface.gameObject.transform.rotation.x != 0 || surface.gameObject.transform.rotation.x != -0.7071058f) && surface.gameObject.transform.rotation.y <= 0)
+                flipX = true;
+            else
+                flipX = false;
+
+            if (surface.gameObject.transform.rotation.x < -0.9f || surface.gameObject.transform.rotation.y < 0)
+                flipY = true;
+            else
+                flipY = false;
+
+            if (surface.gameObject.transform.rotation.z <= 0)
+                flipZ = true;
+            else
+                flipZ = false;
+        }
+        else
+        {
+            surface = null;
+        }
+
         for(int i = 0; i < line.positionCount; i++)
         {
             line.SetPosition(i, laserPoints[i]);
         }
+
+
     }
 
     void TrackMouse()
@@ -46,7 +161,7 @@ public class LaserTurret : MonoBehaviour
         Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         RaycastHit hit;
-        if(Physics.Raycast(cameraRay, out hit, 1000, targetLayer ))
+        if(Physics.Raycast(cameraRay, out hit, 1000, targetLayer))
         {
             crosshair.transform.forward = hit.normal;
             crosshair.transform.position = hit.point + hit.normal * 0.1f;
